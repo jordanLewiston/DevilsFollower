a83629a501327a758027ee7692364e0f677517c9
Jordan Ponomarenko <jordanlewiston4@gmail.com>
2026-02-25 06:57:56 +0500

    wiping

--- Source/DevilsFollower/DevilsFollower.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "DevilsFollower.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, DevilsFollower, "DevilsFollower" );
-
-DEFINE_LOG_CATEGORY(LogDevilsFollower)
